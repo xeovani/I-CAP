eecd04d5f91e073fb39fb2530a12c2410d5e09f7
xeovani <Xeovani@yahoo.com.mx>
2017-08-31 13:38:30 -0500

    ultimo-commit-31/08/2017

--- js/Main.js
+++ js/Main.js
@@ -4,14 +4,9 @@ $(document).ready(function(){
 
          $(".cont_img_tecl").hide().fadeIn(5000);
 
-         $(".text_cuerpo >p").hide().fadeIn(5000);
-         $(".text_cuerpo >H4").hide().fadeIn(5000);
+         $(".text_cuerpo >p").hide().slideDown(5000);
+         $(".text_cuerpo >H4").hide().slideDown(5000);
 
-      /*  $(".menubar .btn-menu").click(function(){
-
-                $("nav").css("left", "0px");
-            return false;
-        });*/
 
         var contador=1;
 
@@ -108,16 +103,84 @@ $(document).ready(function(){
 
 
 
+
+             /*---------------------select cliente----------------------*/
+
+
+                $("#CLIENTE").on('change',function(){
+                    var cliente = $(this).val();
+
+//                     $("#SOLICITANTE").
+
+
+                });
+
+           var CLIENTE= new Array;
+
+           CLIENTE[1]=["Aidé Liliana Lopez Lopez","Alan Arispe Treviño","Alan García Aguilar","Alejandro Flores Ruiz","Ana Isabel Ceja Hernández","Andres Coronado Carréon","Antonio Arias Sotero","Carlos Dávila Salazar","Carlos González Cano","Jesica Elena Olague Martínez","Karen Sarai Pérez Gloria",
+           "Manuel Alenadro Garcia Santos","Marcela Leal Santos","Margarita Castillo Ayala","Nestor Alfonso Lopez Martinez","Norma Elsa Alanis Gonzalez","Pedro Garcia Sanchez","Raul Alejandro Hernadez","","","","",""];
+
+
+
 });
 
 
 
 
-   /* var fecha=new Date;
 
-    console.log(fecha.getHours());
-    console.log(submenu.length);
-   */
+
+       /* function startTime(){
+        today=new Date();
+        h=today.getHours();
+        m=today.getMinutes();
+        s=today.getSeconds();
+        m=checkTime(m);
+        s=checkTime(s);
+        document.getElementById('reloj').value=h+":"+m+":"+s;
+            console.dir(h+":"+m+":"+s);
+        t=setTimeout('startTime()',500);}
+
+        function checkTime(i)
+        {if (i<10) {i="0" + i;}return i;}
+
+        window.onload=function(){startTime();}
+*/
+
+                /*-----------------------------------duracion de sesion--------------*/
+
+
+
+                        var segundos = 0;
+                        var minutos = 0;
+                        var horas = 0;
+                        var t;
+                        var tiempo = 0;
+
+                        function contadorTiempo() {
+                            document.getElementById("Tiempo-sesion").value = horas + ":" + minutos + ":" + segundos;
+                            segundos = segundos + 1;
+                            t = setTimeout(function(){ contadorTiempo() }, 1000);
+                            if(segundos==59){
+                                segundos=0;
+                                minutos= minutos + 1;
+
+                                    if(minutos==59)
+                                        {
+                                            minutos=0;
+                                            horas = horas + 1;
+                                        }
+                            }
+                        }
+
+                        function iniciarConteo() {
+                            if (!tiempo) {
+                                tiempo = 1;
+                                contadorTiempo();
+                            }
+                        }
+
+                        window.onload=iniciarConteo;
+
 
 
 
